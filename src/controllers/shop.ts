fe3c24467d432bb0a65bf241b6c786f74ae33c9c
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-10-21 22:53:12 +0530

    invoice issue fixed

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -140,38 +140,45 @@ export const getInvoices = (
 ) => {
   const orderId = req.params.orderId;
 
-  Order.findById(orderId).then((order) => {
-    if (!order) {
-      return next(new Error("No order found"));
-    }
-    if (order.user.userId.toString() !== req.user._id.toString()) {
-      return next(new Error("Unauthorized"));
-    }
-    const invoiceName = "invoice-" + orderId + ".pdf";
-    const invoicePath = path.join("data", "invoices", invoiceName);
-    const pdfDoc = new PDFDocument();
-    res.setHeader("Content-Type", "application/pdf");
-    pdfDoc.pipe(fs.createWriteStream(invoicePath));
-    pdfDoc.pipe(res);
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found"));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized"));
+      }
 
-    pdfDoc.fontSize(26).text("Invoice", { underline: true });
-    pdfDoc.text("-------------------------------------");
-    let totalPrice = 0;
-    order.products.forEach((prod) => {
-      totalPrice = totalPrice + prod.quantity * Number(prod.product.price);
-      pdfDoc
-        .fontSize(14)
-        .text(
-          prod.product.title +
-            " - " +
-            prod.quantity +
-            " x " +
-            " $ " +
-            prod.product.price
-        );
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      // Ensure the directory exists
+      const dir = path.dirname(invoicePath);
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true }); // Create the directory if it doesn't exist
+      }
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text("Invoice", { underline: true });
+      pdfDoc.text("-------------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * Number(prod.product.price);
+        pdfDoc
+          .fontSize(14)
+          .text(
+            `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`
+          );
+      });
+      pdfDoc.text("--------------");
+      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+      pdfDoc.end();
+    })
+    .catch((err) => {
+      next(err); // Handle any errors that occur during the Order.findById call
     });
-    pdfDoc.text("--------------");
-    pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
-    pdfDoc.end();
-  });
 };
